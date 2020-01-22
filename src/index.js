247163bd5f303dc33812fbf2f489bc5089950908
AntonDrazdou <woodland.by@gmail.com>
2020-01-22 23:24:19 +0300

    add props

--- src/index.js
+++ src/index.js
@@ -6,12 +6,20 @@ import SearchPanel from './components/search_panel'
 import TodoList from './components/todo-list'
 
 const App = () =>{
+
+  const todoData = [
+    {label: 'Drink coffee', important: false, id: 1 },
+    {label: 'Make Awesome App', important: true, id: 2 },
+    {label: 'Have a lunch', important: false, id: 3 }
+  ];
+
+
   return (
     <div>
     
       <AppHeader />
       <SearchPanel />
-      <TodoList />
+      <TodoList todos={todoData}/>
     </div>
   );
 }
