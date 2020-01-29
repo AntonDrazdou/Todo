77e8dc1b51883baaea1f86b931f14f2b9a86ff50
AntonDrazdou <woodland.by@gmail.com>
2020-01-29 22:54:01 +0300

    add feature: delete butti=on work

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AppHeader from '../app-header/app-header';
 import SearchPanel from '../search-panel/search_panel';
@@ -7,13 +7,33 @@ import ItemStatusFilter from '../item-status-filter/item-status-filter';
 
 import './app.css';
 
-const App = () =>{
+export default class App extends Component{
 
-  const todoData = [
-    {label: 'Drink coffee', important: false, id: 1 },
-    {label: 'Make Awesome App', important: true, id: 2 },
-    {label: 'Have a lunch', important: false, id: 3 }
-  ];
+  state = {
+    todoData: [
+      {label: 'Drink coffee', important: false, id: 1 },
+      {label: 'Make Awesome App', important: true, id: 2 },
+      {label: 'Have a lunch', important: false, id: 3 }
+    ]
+  };
+
+deleteItem = (id) => {
+
+  this.setState(( {todoData} )=>{
+    const idx = todoData.findIndex((el) => el.id === id);
+    console.log(idx);
+    todoData.splice(idx, 1);
+
+    const newArray = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)]
+
+    return {
+      todoData: newArray
+    };
+  });
+
+};
+
+render() {
 
   return (
     <div className="todo-app">
@@ -21,10 +41,9 @@ const App = () =>{
       <SearchPanel />
       <ItemStatusFilter />
       <TodoList 
-      todos={todoData}
-      onDeleted={(id) => console.log('del', id)}/>
+      todos={this.state.todoData}
+      onDeleted={ this.deleteItem }/>
     </div>
-  );
-}
-
-export default App;
+      );
+  } 
+};
