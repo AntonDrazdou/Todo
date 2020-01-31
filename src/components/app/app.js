28f6dab8b2ba0027aae5f22ffde01ea21a73df28
AntonDrazdou <woodland.by@gmail.com>
2020-01-31 23:18:25 +0300

    add feature: active done and all buttons is work

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,7 +18,8 @@ export default class App extends Component{
       this.createTodoItem('Make Awesome App'),
       this.createTodoItem('Have a lunch')
     ], 
-    term: ''
+    term: '',
+    filter: 'all'
   };
 
   createTodoItem (label) {
@@ -100,6 +101,10 @@ onSearchChange = (term) => {
   this.setState({ term });
 };
 
+onFilterChange = (filter) => {
+  this.setState({ filter });
+};
+
 search(items, term){
   if (term.length === 0){
     return items; 
@@ -110,11 +115,24 @@ search(items, term){
     });
 }
 
+filter(items, filter) {
+  switch(filter){
+    case 'all':
+      return items;
+    case 'active':
+      return items.filter((item) => !item.done);
+    case 'done':
+      return items.filter((item) => item.done);
+    default: 
+      return items;
+  }
+}
+
 render() {
 
-  const {todoData, term} = this.state;
+  const {todoData, term, filter} = this.state;
 
-  const visibleItems = this.search(todoData, term);
+  const visibleItems = this.filter(this.search(todoData, term), filter);
  
 
   const doneCount = todoData.filter((el) => el.done).length;
@@ -125,7 +143,8 @@ render() {
     <div className="todo-app">
       <AppHeader toDo = {todoCount} done = {doneCount} />
       <SearchPanel onSearchChange={ this.onSearchChange }/>
-      <ItemStatusFilter />
+      <ItemStatusFilter filter={filter}
+      onFilterChange={this.onFilterChange} />
       <TodoList 
       todos={ visibleItems }
       onDeleted={ this.deleteItem }
